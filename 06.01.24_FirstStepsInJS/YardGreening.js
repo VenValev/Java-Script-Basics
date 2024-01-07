07cf830bdbd0269b4bb0bee94759db9854eae03d
VenValev <vencislav67@hotmail.com>
2024-01-07 16:04:47 +0200

    124

--- 06.01.24_FirstStepsInJS/YardGreening.js
+++ 06.01.24_FirstStepsInJS/YardGreening.js
@@ -1,8 +1,10 @@
 function calcPrice(input){
     let sqm = Number(input[0]);
     let tPrice = sqm * 7.61;
-    let disPrice = t.price * 0.18;
+    let disPrice = tPrice * 0.18;
     let fPrice = tPrice - disPrice;
     console.log(`The final price is: ${fPrice} lv.`);
-    console.log(`"The discount is: ${disPrice} lv.`);
+    console.log(`The discount is: ${disPrice} lv.`);
 }
+
+calcPrice(["550"]);
